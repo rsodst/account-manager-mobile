022595470a672265a330f19edb4fdb3c9b4d0d25
Ринат Сафиуллин <rsodst@gmail.com>
2020-06-08 01:47:48 +0500

    feat: fix manifest

--- app/src/main/java/com/modulbank/accountmanager/Environment.settings.kt
+++ app/src/main/java/com/modulbank/accountmanager/Environment.settings.kt
@@ -2,6 +2,6 @@ package com.modulbank.accountmanager
 
 class EnvironmentSettings {
     companion object{
-        val apiUrl : String = "http://192.168.0.144:45459/"
+        val apiUrl : String = "https://account-manager-backend.herokuapp.com/"
     }
 }
